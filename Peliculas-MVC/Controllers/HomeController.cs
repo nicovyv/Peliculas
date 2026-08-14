fc7fd5dc9ffe814fa6c31cf94115ed8355e38529
nicolas zabala <nicozb.v@gmail.com>
2026-08-14 19:16:39 -0300

    filtro por título y género

--- Peliculas-MVC/Controllers/HomeController.cs
+++ Peliculas-MVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Pelicula_MVC.Data;
 using Peliculas_MVC.Models;
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Peliculas_MVC.Controllers
 {
@@ -17,9 +18,46 @@ namespace Peliculas_MVC.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1, string txtBusqueda = "", int generoId = 0)
         {
-            var peliculas = await _context.Peliculas.ToListAsync();
+            const int pageSize = 8;
+
+            var consulta = _context.Peliculas.AsQueryable();
+            if (!string.IsNullOrEmpty(txtBusqueda))
+            {
+                consulta = consulta.Where(p => p.Titulo.Contains(txtBusqueda));
+            }
+
+            if (generoId > 0)
+            {
+                consulta = consulta.Where(p => p.GeneroId == generoId);
+            }
+
+
+            var totalCount = await consulta.CountAsync();
+            var peliculas = await consulta
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            ViewBag.PageNumber = page;
+            ViewBag.PageSize = pageSize;
+            ViewBag.TotalCount = totalCount;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            ViewBag.HasPreviousPage = page > 1;
+            ViewBag.HasNextPage = page < ViewBag.TotalPages;
+            ViewBag.Consulta = txtBusqueda;
+
+            var generos = await _context.Generos.OrderBy(g => g.Descripcion).ToListAsync();
+            generos.Insert(0, new Genero { Id = 0, Descripcion = "Todos" });
+
+            ViewBag.GeneroId = new SelectList(
+                generos,
+                "Id",
+                "Descripcion",
+                generoId
+             );
+
             return View(peliculas);
         }
 
